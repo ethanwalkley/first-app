e1504a404003e3b771720005d5558ac0d949d11a
ethanwalkley <ewalkley96@gmail.com>
2019-02-15 20:45:02 -0800

    Updates

---
+++ precache-manifest.5a5bfebe8731116ef30eed0765b23ca8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "14bb2cbe8615b53b02e4",
+    "url": "/first-app/static/css/main.380de256.chunk.css"
+  },
+  {
+    "revision": "14bb2cbe8615b53b02e4",
+    "url": "/first-app/static/js/main.14bb2cbe.chunk.js"
+  },
+  {
+    "revision": "57578587519aac8b8a35",
+    "url": "/first-app/static/js/1.57578587.chunk.js"
+  },
+  {
+    "revision": "7f2988afc7cd3ef40830",
+    "url": "/first-app/static/js/runtime~main.7f2988af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/first-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "af62d55e5c6f37a6b1a4332fe08482e4",
+    "url": "/first-app/index.html"
+  }
+];
