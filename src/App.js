86da65bdb089dbfb51af2e4117a2c06e455a1e6c
ethanwalkley <ewalkley96@gmail.com>
2019-02-15 20:29:19 -0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PubNubReact from 'pubnub-react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.pubnub = new PubNubReact({
+      publishKey: 'npm -g create-react-app',
+      subscribeKey: 'npm -g create-react-app'
+    }
+    );
+    this.pubnub.init(this);
+    this.state = {
+      headerText: "Hello world!"
+    }
+  }
+
+
   render() {
+    console.log("hello World");
     return (
       <div className="App">
+        <img src="../images/Group_2.jpg" ></img>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            {this.state.headerText}
           </p>
           <a
             className="App-link"
@@ -17,7 +34,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            This is so cool!
           </a>
         </header>
       </div>
